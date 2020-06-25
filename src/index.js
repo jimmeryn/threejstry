9bd8609db54704a59448a244deddc884cca6cdcc
jimmeryn <jimmeryn@gmail.com>
2020-06-25 14:39:16 +0200

    gsap anim

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./style/style";
+import gsap from "gsap";
+import * as THREE from "three";
 
 import { createInitialRendererObjects } from "./init";
 import { createPointLight } from "./lights";
@@ -9,9 +11,11 @@ const rendererObjects = createInitialRendererObjects();
 function onKeyDown(event) {
   const keyCode = event.which;
   if (keyCode == 87) {
-    rendererObjects.nucleus.position.z -= 10;
+    rendererObjects.nucleus.position.z -= 5;
+    tl.to(rendererObjects.nucleus.position, 0.1, { y: 1, delay: 0.3 });
+    tl.to(rendererObjects.nucleus.position, 0.1, { y: -10 });
   } else if (keyCode == 83) {
-    rendererObjects.nucleus.position.z += 10;
+    rendererObjects.nucleus.position.z += 5;
   } else if (keyCode == 65) {
     rendererObjects.nucleus.position.x -= 1;
   } else if (keyCode == 68) {
@@ -35,6 +39,7 @@ function init() {
   sphere.translateX(15);
   sphere.translateY(-11);
   rendererObjects.nucleus = nucleus;
+  rendererObjects.sphere = sphere;
   rendererObjects.scene.add(nucleus, l1, l2, sphere);
 
   document.body.appendChild(rendererObjects.renderer.domElement);
@@ -56,9 +61,22 @@ const render = () => {
 
 const animate = () => {
   requestAnimationFrame(animate);
-  // rendererObjects.nucleus.translateZ(-1);
   render();
 };
 
 init(rendererObjects);
+
+const ray = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+function mouseHover(event) {
+  event.preventDefault();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  ray.setFromCamera(mouse, rendererObjects.camera);
+  const intersects = ray.intersectObjects(rendererObjects.scene.children, true);
+  intersects.forEach(e => e.object.material.color.set(0xff0000));
+}
+window.addEventListener("mousemove", mouseHover);
+let tl = gsap.timeline({ defaults: { ease: "Power3.inOut" } });
+tl.to(rendererObjects.sphere.scale, 1, { x: 1, y: 1, z: 1 });
 animate(rendererObjects);
